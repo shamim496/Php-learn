df3b3f02f53d0b971656f6e5dc0cbf5480e04c7e
Shamim Hasan <132350550+shamim496@users.noreply.github.com>
2023-10-14 13:37:13 +0600

    Add files via upload

---
+++ pratice.php
@@ -0,0 +1,190 @@
+<?php
+
+//$password = '123456';
+//if($password == '123456'){
+//	echo "Login Success";
+//}else{
+//	echo "Login Failed";	
+//}
+// $a = 10;
+// $b = 20;
+// if($a > $b){
+// 	echo "b is Greater than a";
+// }else{
+// 	echo "a is Greater than b";
+// }
+// $number =78;
+// if ($number > 100){
+// echo "Input invaild.";
+// }elseif ($number >=80){
+// 	echo "A+";
+// }elseif ($number >=70){
+// 	echo "A";
+// }elseif ($number >=60){
+// 	echo "A-";
+// }elseif ($number >=50){
+// 	echo "B";
+// }elseif ($number >=40){
+// 	echo "c";
+// }elseif ($number >=33){
+// 	echo "D";
+// }else{
+// 	echo "failed";	
+// }
+
+
+
+// $old = 70
+// if ($old >70);{
+// 	echo "senior cizition";
+// }elseif ($old >=45){
+// 	echo "Mature";
+// }elseif ($old >=25){
+// 	echo "young";
+// }elseif ($old >=10){
+// 	echo "Tenager";
+// }elseif ($old >=5){
+// 	echo "child";
+// }else{
+// 	"Not born";
+// }
+
+
+
+
+
+
+// $number = 100;
+// switch($number){
+// 	case '50':
+// 		echo "your number is 50";
+// 		echo "<style>body{background-color:blue;}</style>";
+// 	break;
+
+// 	case '60':
+// 		echo "your number is 60";
+// 		echo "<style>body{background-color:red;}</style>";
+// 	break;
+
+// 	case '70':
+// 		echo "your number is 70";
+// 		echo "<style>body{background-color:skyblue;}</style>";
+// 	break;
+
+// 	case '80':
+// 		echo "your number is 80";
+// 		echo "<style>body{background-color:green;}</style>";
+// 	break;
+
+// 	case '90':
+// 		echo "your number is 90";
+// 		echo "<style>body{background-color:green;}</style>";
+// 	break;
+
+// 	case '100';
+// 		echo "your number is 100";
+// 		echo "<style>body{background-color:lightblue;}</style>";
+// 	break;
+
+// 	default:
+// 	echo "Not found";
+// 	break;
+// }
+
+
+
+
+// $number = 100;
+// switch ($number){
+// 	case '50':
+// 		echo "your number is 50";
+// 	break;
+
+// 	case '70':
+// 		echo "your number is 70";
+// 	break;
+
+// 	case '80':
+// 		echo "your name is 80";
+// 	break;
+
+// 	case '90':
+// 		echo "your number is 90";
+// 	break;
+
+// 	default:
+// 	 	echo "Not found";
+// 	 }
+
+
+
+// $old =70;
+// if ($old >70){
+// 	echo "senior cizition";
+// }elseif ($old >=45){
+// 	echo "Mature";
+// }elseif ($old >=25){
+// 	echo "young";
+// }elseif ($old >=10){
+// 	echo "Tenager";
+// }elseif ($old>=5){
+// 	echo "Child";
+// }else{
+// 	echo "Not born";
+// }
+
+
+
+// $password = '123456';
+// if($password == '123456'){
+// 	echo "Login success";
+// }else{
+// 	echo "Login Failed";
+// }
+
+
+// $password = '123456';
+// if($password == '1234567'){
+// 	echo "Login success";
+// }else{
+// 	echo "Login Falid";
+// }
+
+
+// $color = red;
+// switch($color){
+// 	case 'red':
+// 		echo "your favourite color is red";
+// 	break;
+
+// 	case 'green':
+// 		echo "your favourite color is green";
+// 	break;
+
+// 	case 'light greeen':
+// 		echo "your favourite color is light green";
+// 	break;
+
+// 	case 'light blue':
+// 		echo "your favourite color is light blue";
+// 	break;
+
+// 	case 'black':
+// 		echo "your favourite color is black";
+// 	break;
+
+// 	case 'white':
+// 		echo "your favourite color is white";
+// 	break;
+
+// 	default:
+// 		echo "your favourite color is not found";
+// }
+
+
+
+
+
+// $date = 'monday';
+// echo date('D');
+
